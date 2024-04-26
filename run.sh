1c9583d188ca33520d66312c2c60163dc766b2fa
Iker Jansa <Iker.jansa@gmail.com>
2024-04-26 20:46:10 +0200

    New features to the model

---
+++ run.sh
@@ -0,0 +1,26 @@
+
+#! /bin/bash
+# Activate the venv with $ source DDI_env/Scripts/activate
+
+BASEDIR="."
+
+./corenlp-server.sh -quiet true -port 9000 -timeout 15000  &
+sleep 1
+
+# extract features
+echo "Extracting features"
+python extract-features.py $BASEDIR/data/devel/ > devel.cod &
+python extract-features.py $BASEDIR/data/train/ | tee train.cod | cut -f4- > train.cod.cl
+
+kill `cat /tmp/corenlp-server.running`
+
+# train model
+echo "Training model"
+python train-sklearn.py model.joblib vectorizer.joblib < train.cod.cl
+# run model
+echo "Running model..."
+python predict-sklearn.py model.joblib vectorizer.joblib < devel.cod > devel.out
+# evaluate results
+echo "Evaluating results..."
+python evaluator.py DDI $BASEDIR/data/devel/ devel.out > devel.stats
+
